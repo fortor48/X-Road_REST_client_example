ed74b59476eb09383cdaefb6c38097d0d22b933d
kir <kirill@MacBook-Pro-kirill.local>
2024-06-23 08:32:10 +0300

    Added asic download page, download asic from Security server. Separate navbar code to separate file.

--- app.py
+++ app.py
@@ -1,85 +1,143 @@
 import sys
+from datetime import datetime
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_bootstrap import Bootstrap
-import requests
-from urllib.parse import quote
-import chardet
-import io
-import httpx
-import json
 import utils
-import config_utils
+import os
 
 
 
 # Установка правильной кодировки для stdout
 #sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+crt_directory = "certs"
+asic_derectory = "asic"
+key = "key.pem"
+cert = "crt.pem"
+
+ASIC_DIR = os.path.join(os.getcwd(), 'asic')
+
+# Проверяем, существует ли директория
+if not os.path.exists(crt_directory):
+    # Создаем директорию, если её нет
+    os.makedirs(crt_directory)
+    print(f"Директория '{crt_directory}' была создана.")
+else:
+    print(f"Директория '{crt_directory}' уже существует.")
+
+# Проверяем, существует ли директория
+if not os.path.exists(asic_derectory):
+    # Создаем директорию, если её нет
+    os.makedirs(asic_derectory)
+    print(f"Директория '{asic_derectory}' была создана.")
+else:
+    print(f"Директория '{asic_derectory}' уже существует.")
+
+private_key_full_path = f"{crt_directory}/{key}"
+certificate_full_path = f"{crt_directory}/{cert}"
+
+if not os.path.exists(private_key_full_path) or not os.path.exists(certificate_full_path):
+    utils.generate_key_cert(key, cert, crt_directory)
+
+
+def get_files_with_metadata(directory):
+    files_metadata = []
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        creation_time = os.path.getctime(filepath)
+        creation_time_str = datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d %H:%M:%S')
+        files_metadata.append({
+            'name': filename,
+            'creation_time': creation_time_str
+        })
+    return files_metadata
+
 
 app = Flask(__name__)
 Bootstrap(app)  # Инициализация Flask-Bootstrap4
 
 conf = utils.Config('config.ini')
 
-#base_url = "http://127.0.0.1:8000/person/name/"
-
 
 @app.route('/', methods=['GET', 'POST'])
 def serch_user():
-    if request.method == 'POST':
+    if request.method == 'POST': # Прийшов запит з даними від форми (на пошук за одним з параметрів), обробляемо
         print("test")
         search_field = request.form.get('search_field')
         search_value = request.form.get('search_value')
 
         try:
-            data = utils.get_person_from_service(search_field, search_value)
+            data = utils.get_person_from_service(search_field, search_value, conf)
         except Exception as e:
-            return render_template('error.html', error_message=e)
-        return render_template('list_person.html', data=data)
+            return render_template('error.html', error_message=e,  current_page='index')
+        return render_template('list_person.html', data=data,  current_page='index')
 
-    return render_template('search_form.html')
+    return render_template('search_form.html',  current_page='index') # Прийшов GET запит, просто віддаємо веб сторінку
 
 
 @app.route('/create', methods=['GET', 'POST'])
 def create_user():
-    if request.method == 'POST':
+    if request.method == 'POST': # Прийшов запит з даними від форми (на створеня особи), обробляемо
         try:
             form_data = request.get_json()
 
-            response = utils.service_add_person(form_data)
+            response = utils.service_add_person(form_data, conf)
             resp = jsonify({"message": response.body}), response.status_code
             return resp
         except Exception as e:
             resp = jsonify(message='Ошибка при создании записи: {}'.format(str(e))), 422
             return resp
 
-    return render_template('create_person.html')
+    return render_template('create_person.html',  current_page='create') # Прийшов GET запит, просто віддаємо веб сторінку
 
 
 @app.route('/edit', methods = ['POST'])
 def edit_user():
     data = request.get_json()
     try:
-        http_resp = utils.edit_person_in_service(data)
+        http_resp = utils.edit_person_in_service(data, conf)
     except Exception as e:
         resp = jsonify({f"message": "Data NOT received successfully", "error": e}), 500
         return resp
     return jsonify({"message": http_resp.body}), http_resp.status_code
-
-
-
 
 
 @app.route('/delete', methods = ['POST'])
 def delete_person():
     data = request.get_json()
     try:
-        http_resp = utils.service_delete_person(data)
+        http_resp = utils.service_delete_person(data, conf)
     except Exception as e:
         resp = jsonify({f"message": "Data NOT received successfully", "error": e}), 500
         return resp
     return jsonify({"message": http_resp.body}), http_resp.status_code
 
+@app.route('/files')
+def list_files():
+    try:
+        files = []
+        for filename in os.listdir(asic_derectory):
+            filepath = os.path.join(asic_derectory, filename)
+            if os.path.isfile(filepath):
+                creation_time = datetime.fromtimestamp(os.path.getctime(filepath))
+                files.append({
+                    'name': filename,
+                    'creation_time': creation_time.strftime('%Y-%m-%d %H:%M:%S')
+                })
+
+        # Sort files by creation time (descending order)
+        files = sorted(files, key=lambda x: x['creation_time'], reverse=True)
+
+        return render_template('list_files_run_away.html', files=files, current_page='files')
+    except Exception as e:
+        return render_template('error.html', error_message=e, current_page='files')
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    try:
+        return send_from_directory(ASIC_DIR, filename, as_attachment=True)
+    except Exception as e:
+        return render_template('error.html', error_message=e, current_page='files')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
+import configparser
+import uuid
+import re
+from urllib.parse import quote
 import requests
 from requests import Response
-from urllib.parse import quote
-import configparser
 
-base_url = "http://127.0.0.1:8000/person/"
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+import datetime
 
 
 class Response:
@@ -14,51 +21,201 @@ class Response:
     def __repr__(self):
         return f"Response(status_code={self.status_code}, body={self.body})"
 
+
 class Config:
     def __init__(self, filename):
         self.parser = configparser.ConfigParser()
         self.parser.read(filename)
 
         # Присваивание конфигурационных параметров как атрибутов класса
-        self.trembita_host    = self.parser.get('trembita', 'host')
+        self.trembita_protocol = self.parser.get('trembita', 'protocol')
+        self.trembita_host = self.parser.get('trembita', 'host')
+        self.cert_path = self.parser.get('trembita', 'cert_path')
         self.trembita_purpose = self.parser.get('trembita', 'purpose_id')
 
-        self.client_org_type  = self.parser.get('client', 'type')
-        self.client_org_code  = self.parser.get('client', 'code')
-        self.client_org_sub   = self.parser.get('client', 'subsystem')
+        self.client_instance = self.parser.get('client', 'instance_name')
+        self.client_org_type = self.parser.get('client', 'member_class')
+        self.client_org_code = self.parser.get('client', 'member_code')
+        self.client_org_sub = self.parser.get('client', 'subsystem_code')
 
-        self.service_org_type = self.parser.get('service', 'type')
-        self.service_org_code = self.parser.get('service', 'code')
-        self.service_org_sub  = self.parser.get('service', 'subsystem')
-        self.service_org_name = self.parser.get('service', 'name')
-        self.service_org_version = self.parser.get('service', 'ver')
+        self.service_instance = self.parser.get('service', 'instance_name')
+        self.service_org_type = self.parser.get('service', 'member_class')
+        self.service_org_code = self.parser.get('service', 'member_code')
+        self.service_org_sub = self.parser.get('service', 'subsystem_code')
+        self.service_org_name = self.parser.get('service', 'service_code')
+        self.service_org_version = self.parser.get('service', 'service_version')
 
     def get(self, section, option):
         return self.parser.get(section, option)
 
-    def get_uxp_client_header(self) -> str:
-        result = self.client_org_type + "/" + self.client_org_code + "/" + self.client_org_sub
-        return result
 
-    def get_uxp_service_header(self) -> str:
-        result = (self.service_org_type + "/" + self.service_org_code + "/" +
-                  self.service_org_sub + "/" + self.service_org_name + "/" + self.service_org_version)
-        return result
+def download_asic_from_trembita(asics_dir: str, queryId: str, config_instance):
+    # https: // sec1.gov / signature? & queryId = abc12345 & xRoadInstance = EE & memberClass = ENT & memberCode =
+    # CLIENT1 & subsystemCode = SUB
+    query_params = {
+        "queryId": queryId,
+        "xRoadInstance": config_instance.client_instance,
+        "memberClass": config_instance.client_org_type,
+        "memberCode": config_instance.client_org_code,
+        "subsystemCode": config_instance.client_org_sub
+    }
+
+    if config_instance.trembita_protocol == "https":
+        url = f"https://{config_instance.trembita_host}/signature"
+
+        # Отправляем GET-запрос для скачивания файла
+        response = requests.get(url, stream=True, params=query_params,
+                                cert=(f"{config_instance.cert_path}/crt.pem", f"{config_instance.cert_path}/key.pem"),
+                                verify=f"{config_instance.cert_path}/trembita.pem")
+
+        # Проверяем, успешно ли выполнен запрос
+        if response.status_code == 200:
+            # Пытаемся получить имя файла из заголовка Content-Disposition
+            content_disposition = response.headers.get('Content-Disposition')
+            if content_disposition:
+                # Паттерн для извлечения имени файла
+                filename_match = re.findall('filename="(.+)"', content_disposition)
+                if filename_match:
+                    local_filename = filename_match[0]
+                else:
+                    local_filename = 'downloaded_file.ext'
+            else:
+                # Если заголовка нет, используем имя по умолчанию
+                local_filename = 'downloaded_file.ext'
+
+            # Открываем локальный файл в режиме записи байтов
+            with open(f"{asics_dir}/{local_filename}", 'wb') as file:
+                # Проходим по частям ответа и записываем их в файл
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print(f'Файл успешно скачан и сохранен как {local_filename}')
+        else:
+            print(f'Не удалось скачать файл. Статус код: {response.status_code}')
+    else:
+        raise ValueError("This function works only when trembita_protocol is https!!")
 
 
-config = Config('config.ini')
+def generate_key_cert(key: str, crt: str, path: str):
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
 
-def get_uxp_client_header()-> str:
-    result = config.get
+    # Создание объекта для сертификата
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, "UA"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Kiev"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, "Kiev"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, "The Best Company"),
+        x509.NameAttribute(NameOID.COMMON_NAME, "test.com"),
+    ])
+
+    # Создание самоподписанного сертификата
+    cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        private_key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        # Сертификат будет действителен в течение одного года
+        datetime.datetime.utcnow() + datetime.timedelta(days=365)
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName("test.com")]),
+        critical=False,
+    ).sign(private_key, hashes.SHA256())
+
+    # Сохранение приватного ключа в файл
+    key_full_path = f"{path}/{key}"
+
+    with open(key_full_path, "wb") as f:
+        f.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
+
+    # Сохранение сертификата в файл
+    crt_full_path = f"{path}/{crt}"
+
+    with open(crt_full_path, "wb") as f:
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
 
 
-def get_person_from_service(parameter: str, value: str) -> list:
-    url = base_url + parameter + "/" + value
+def get_uxp_headers_from_config(config_instance) -> dict:
+    uxp_client_header_name = "UXP-Client"
+    uxp_service_header_name = "UXP-Service"
+
+    uxp_client_header_value = (
+        f"{config_instance.client_instance}/"
+        f"{config_instance.client_org_type}/"
+        f"{config_instance.client_org_code}/"
+        f"{config_instance.client_org_sub}"
+    )
+
+    base_service_value = (
+        f"{config_instance.service_instance}/"
+        f"{config_instance.service_org_type}/"
+        f"{config_instance.service_org_code}/"
+        f"{config_instance.service_org_sub}/"
+        f"{config_instance.service_org_name}"
+    )
+
+    if config_instance.service_org_version:
+        uxp_service_header_value = f"{base_service_value}/{config_instance.service_org_version}"
+    else:
+        uxp_service_header_value = base_service_value
+
+    return {
+        uxp_client_header_name: uxp_client_header_value,
+        uxp_service_header_name: uxp_service_header_value
+    }
+
+
+def get_uxp_query_params() -> dict:
+    uxp_query_id_name = "queryId"
+    uxp_user_id_name = "userId"
+
+    uxp_query_id_value = str(uuid.uuid4())
+    uxp_user_id_value = "Flask_client"  # можна задати будьяке значення, при обміні це імʼя
+    # буде збережено на ШБО, та його можна буде побачити наприклад за допомогою веріфаєра.
+
+    return {
+        uxp_query_id_name: uxp_query_id_value,
+        uxp_user_id_name: uxp_user_id_value
+    }
+
+
+def get_base_trembita_uri(config_instance) -> str:
+    if config_instance.trembita_protocol == "https":
+        uri = f"https://{config_instance.trembita_host}/restapi"
+    else:
+        uri = f"http://{config_instance.trembita_host}/restapi"
+    return uri
+
+
+def get_person_from_service(parameter: str, value: str, config_instance) -> list:
+    base_uri = get_base_trembita_uri(config_instance)
+    headers = get_uxp_headers_from_config(config_instance)
+    query_params = get_uxp_query_params()
+
+    url = f"{base_uri}/{parameter}/{value}"
     encoded_url = quote(url, safe=':/')
     try:
-        response = requests.get(encoded_url)
+        if config_instance.trembita_protocol == "https":
+            response = requests.get(encoded_url, headers=headers, params=query_params,
+                                    cert=(f"{config_instance.cert_path}/crt.pem", f"{config_instance.cert_path}/key.pem"),
+                                    verify=f"{config_instance.cert_path}/trembita.pem")
+        else:
+            response = requests.get(encoded_url, headers=headers, params=query_params)
+
+        download_asic_from_trembita("asic", query_params.get('queryId'), config_instance)
     except Exception as e:
-        raise ValueError("Error while sending HTTP GET")
+        raise ValueError(f"Error while sending HTTP GET: {e}")
 
     if response.status_code == 200:
         json_data = response.json()
@@ -68,11 +225,20 @@ def get_person_from_service(parameter: str, value: str) -> list:
     raise ValueError(f"Recieved HTTP code: {response.status_code}, error message: {response.text}")
 
 
-def edit_person_in_service(data: dict) -> Response:
+def edit_person_in_service(data: dict, config_instance) -> Response:
+    base_url = get_base_trembita_uri(config_instance)
+    headers = get_uxp_headers_from_config(config_instance)
+    query_params = get_uxp_query_params()
     url = base_url
+
     try:
-        response = requests.put(url, json=data)
-        #response.raise_for_status()  # Это вызовет исключение, если статус код будет 4xx или 5xx
+        if config_instance.trembita_protocol == "https":
+            response = requests.put(url, json=data, headers=headers, params=query_params,
+                                    cert=(f"{config_instance.cert_path}/crt.pem", f"{config_instance.cert_path}/key.pem"),
+                                    verify=f"{config_instance.cert_path}/trembita.pem")
+        else:
+            response = requests.put(url, json=data, headers=headers, params=query_params)
+
     except requests.exceptions.RequestException as e:
         raise ValueError(f"Error while sending HTTP PUT: {e}")
 
@@ -83,11 +249,19 @@ def edit_person_in_service(data: dict) -> Response:
     return Response(status_code=response.status_code, body=response.json())
 
 
-def service_delete_person(data: dict) -> Response:
+def service_delete_person(data: dict, config_instance) -> Response:
+    base_url = get_base_trembita_uri(config_instance)
+    headers = get_uxp_headers_from_config(config_instance)
+    query_params = get_uxp_query_params()
 
-    url = base_url + "unzr/" + str(data["unzr"])
+    url = base_url + "/unzr/" + str(data["unzr"])
     try:
-        response = requests.delete(url)
+        if config_instance.trembita_protocol == "https":
+            response = requests.delete(url, headers=headers, params=query_params,
+                                    cert=(f"{config_instance.cert_path}/crt.pem", f"{config_instance.cert_path}/key.pem"),
+                                    verify=f"{config_instance.cert_path}/trembita.pem")
+        else:
+            response = requests.delete(url, headers=headers, params=query_params)
     except Exception as e:
         json_body = {"Error while sending HTTP DELETE": f"{e}"}
         return Response(status_code=500, body=json_body)
@@ -95,11 +269,19 @@ def service_delete_person(data: dict) -> Response:
     return Response(status_code=response.status_code, body=response.json())
 
 
-def service_add_person(data: dict) -> Response:
+def service_add_person(data: dict, config_instance) -> Response:
+    base_url = get_base_trembita_uri(config_instance)
+    headers = get_uxp_headers_from_config(config_instance)
+    query_params = get_uxp_query_params()
 
     url = base_url
     try:
-        response = requests.post(url, json=data)
+        if config_instance.trembita_protocol == "https":
+            response = requests.post(url, json=data, headers=headers, params=query_params,
+                                    cert=(f"{config_instance.cert_path}/crt.pem", f"{config_instance.cert_path}/key.pem"),
+                                    verify=f"{config_instance.cert_path}/trembita.pem")
+        else:
+            response = requests.post(url, json=data, headers=headers, params=query_params)
     except Exception as e:
         json_body = {"Error while sending HTTP POST": f"{e}"}
         return Response(status_code=500, body=json_body)
