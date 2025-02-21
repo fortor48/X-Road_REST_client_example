1ebf17036bb75615af6337ebe3b02462fa6020ff
kshypachov <128974084+kshypachov@users.noreply.github.com>
2025-02-21 16:24:30 +0200

    Changed type of gen certificates to ECDSA

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
 import datetime
 import os
@@ -175,7 +176,7 @@ def download_asic_from_trembita(queryId: str, config_instance):
         raise ValueError("Ця функція процює тільки якщо протокол роботи з ШБО Трембіти - https")
 
 
-def generate_key_cert(key: str, crt: str, path: str):
+def generate_key_cert_rsa(key: str, crt: str, path: str):
     # Генерація особистого ключа та сертифіката
     logger.info("Генерація ключа та сертифіката")
     logger.debug(f"Імʼя файлу ключа: {key}, імʼя файлу сертифіката: {crt}, директорія: {path}")
@@ -233,6 +234,62 @@ def generate_key_cert(key: str, crt: str, path: str):
         logger.error(f"Помилка під час збереження ключа або сертифікату: {e}")
         raise
 
+
+def generate_key_cert(key: str, crt: str, path: str):
+    # Генерація особистого ключа та сертифіката
+    logger.info("Генерація ключа та сертифіката ECDSA")
+    logger.debug(f"Імʼя файлу ключа: {key}, імʼя файлу сертифіката: {crt}, директорія: {path}")
+
+    # Генерація приватного ключа ECDSA
+    private_key = ec.generate_private_key(ec.SECP256R1())
+
+    # Створення x509 об'єкту для сертифікату
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, "UA"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Kyiv"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, "Kyiv"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, "The Best Company"),
+        x509.NameAttribute(NameOID.COMMON_NAME, "test.com"),
+    ])
+
+    # Створення самопідписаного сертифікату
+    cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        private_key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        # Сертифікат буде чинним протягом одного року
+        datetime.datetime.utcnow() + datetime.timedelta(days=365)
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName("test.com")]),
+        critical=False,
+    ).sign(private_key, hashes.SHA256())
+
+    # Збереження особистого ключа у файл
+    key_full_path = os.path.join(path, key)
+    try:
+        with open(key_full_path, "wb") as f:
+            f.write(private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+        logger.info(f"Ключ збережено у {key_full_path}")
+
+        # Збереження сертифіката у файл
+        crt_full_path = os.path.join(path, crt)
+        with open(crt_full_path, "wb") as f:
+            f.write(cert.public_bytes(serialization.Encoding.PEM))
+        logger.info(f"Сертифікат збережено у {crt_full_path}")
+    except IOError as e:
+        logger.error(f"Помилка під час збереження ключа або сертифікату: {e}")
+        raise
 
 def get_uxp_headers_from_config(config_instance) -> dict:
     # Формування заголовків UXP для запитів
