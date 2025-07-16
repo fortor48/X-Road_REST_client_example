d65f74c38ba9e15d9379f3a4426d3e937f2a8952
kshypachov <128974084+kshypachov@users.noreply.github.com>
2025-07-16 09:26:10 +0600

    Add en translations

--- generate_certs.sh
+++ generate_certs.sh
@@ -10,16 +10,16 @@ CERTS_DIR="./certs"
 
 KEY_FILE="$CERTS_DIR/$KEY_FILE_NAME"       # PKCS#8
 CERT_FILE="$CERTS_DIR/$CERT_FILE_NAME"
-SUBJ="/C=UA/ST=Kyiv/L=Kyiv/O=Trembita/OU=Dev/CN=localhost"
+SUBJ="/C=UA/ST=Kyiv/L=Kyiv/O=X-Road/OU=Dev/CN=localhost"
 
-# Створення директорії
+# Create the directory
 mkdir -p "$CERTS_DIR"
 
-# Генерація ECDSA ключа у форматі PKCS#8
+# Generate an ECDSA private key in PKCS#8 format
 openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:prime256v1 -out "$KEY_FILE"
 
-# Генерація самопідписаного сертифіката
+# Generate a self-signed certificate
 openssl req -new -x509 -key "$KEY_FILE" -out "$CERT_FILE" -days 365 -subj "$SUBJ"
 
-# Повідомлення
-echo -e "${GREEN}Сертифікат та ключ (${CERT_FILE_NAME}, ${KEY_FILE_NAME}) згенеровано, у директорії ${CERTS_DIR}. Можете додати сертифікат Трембіти та запустити збірку Докер контейнеру, імʼя фалу повинно передаватись у змінній оточення TREMBITA_CERT_FILE.${RESET}"
+# Message
+echo -e "${GREEN}Certificate and key (${CERT_FILE_NAME}, ${KEY_FILE_NAME}) have been generated in the ${CERTS_DIR} directory. You can now add the Trembita certificate and start the Docker container build. The file name should be passed via the TREMBITA_CERT_FILE environment variable.${RESET}"
