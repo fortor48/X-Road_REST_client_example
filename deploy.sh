bbf020c2eb65f4ab7fa45fb1ead37f399c71c28a
kshypachov <128974084+kshypachov@users.noreply.github.com>
2025-07-16 09:14:02 +0600

    Add en translations

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# Створення Unit файлу системної служби для systemd
+# Creating a systemd unit file for the system service
 SERVICE_NAME=x-road_rest_client_example
 SERVICE_FILE=/etc/systemd/system/$SERVICE_NAME.service
 GIT_REPO=https://github.com/fortor48/X-Road_REST_client_example.git
 WORKING_FOLDER=X-Road_REST_client_example
 
-# Оновлення пакетів і встановлення необхідних залежностей
+# Updating packages and installing required dependencies
 sudo apt update
 sudo apt install -y git python3 python3-pip python3-venv
 
-# Клонування репозиторію
+# Cloning the repository
 git clone $GIT_REPO
 cd $WORKING_FOLDER
 
-# Створення та активація віртуального оточення
+# Creating and activating a virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-# Встановлення залежностей
+# Installing dependencies
 pip install -r requirements.txt
 
-
+# Creating the systemd unit file
 sudo bash -c "cat > $SERVICE_FILE" <<EOL
 [Unit]
 Description=Flask Application
@@ -38,13 +38,12 @@ Restart=always
 WantedBy=multi-user.target
 EOL
 
-# Перезавантаження systemd для застосування змін
+# Reloading systemd to apply the changes
 sudo systemctl daemon-reload
 
-# Увімкнення та запуск вебклієнту
+# Enabling and providing instructions to start the web client
 sudo systemctl enable $SERVICE_NAME
 echo "✔️ Service $SERVICE_NAME is enabled."
 echo "ℹ️  Please fill the configuration file if needed."
 echo "▶️  Start service with:"
 echo "    sudo systemctl start $SERVICE_NAME"
-
